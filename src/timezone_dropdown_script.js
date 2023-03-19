4f5a70f6918abf0bd7bef94c17d31fe66122dc5f
zaydogugoker <108925975+zaydogugoker@users.noreply.github.com>
2023-03-19 01:25:11 +0100

    comleted function and variables for each option

--- src/timezone_dropdown_script.js
+++ src/timezone_dropdown_script.js
@@ -1,11 +1,21 @@
 let parisZone = moment().tz("Europe/Paris").format("dddd, MMMM D, YYYY h:mm A");
+let tokyoZone = moment().tz("Asia/Tokyo").format("dddd, MMMM D, YYYY h:mm A");
+let sydneyZone = moment()
+  .tz("Australia/Sydney")
+  .format("dddd, MMMM D, YYYY h:mm A");
+let selectCountries = document.querySelector("#countries");
 
 function displayDate(event) {
   if (event.target.value.length > 0) {
     if (event.target.value == "paris")
       alert(`It is ${parisZone} in Europe/Paris`);
   }
-}
+  if (event.target.value == "tokyo") {
+    alert(`It is ${tokyoZone} in Asia/Tokyo`);
+  }
 
-let selectCountries = document.querySelector("#countries");
+  if (event.target.value == "sydney") {
+    alert(`It is ${sydneyZone} in Australia/Sydney`);
+  }
+}
 selectCountries.addEventListener("change", displayDate);
