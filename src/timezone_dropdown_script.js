0a8e5d42ffb46baa79957f0b56dbb19449167122
zaydogugoker <108925975+zaydogugoker@users.noreply.github.com>
2023-03-19 01:17:01 +0100

    added variables and function for first option

--- src/timezone_dropdown_script.js
+++ src/timezone_dropdown_script.js
@@ -1,1 +1,11 @@
-alert("Hello! I am an alert box!!");
+let parisZone = moment().tz("Europe/Paris").format("dddd, MMMM D, YYYY h:mm A");
+
+function displayDate(event) {
+  if (event.target.value.length > 0) {
+    if (event.target.value == "paris")
+      alert(`It is ${parisZone} in Europe/Paris`);
+  }
+}
+
+let selectCountries = document.querySelector("#countries");
+selectCountries.addEventListener("change", displayDate);
